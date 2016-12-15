1a500599ecd8e2e9ac2852c75ce1086f17a453b5
Joan Oliva <joliva@oneboxtm.com>
2016-12-15 17:12:13 +0100

    pro-infra config added.

--- telegram.go
+++ telegram.go
@@ -83,7 +83,7 @@ func processMessage( message telebot.Message )  {
 
 		switch words[0] {
 		case HELP:
-			bot.SendMessage(message.Chat, "version 1.1.7\nBot commands available are:\n/help\n/list\n/start {alert_name}\n/stop {alert_name}\n/status\n/pod-doublecheck {>0: new_refresh_time_sec/<=0: is to disable}", nil)
+			bot.SendMessage(message.Chat, "version 1.1.10\nBot commands available are:\n/help\n/list\n/start {alert_name}\n/stop {alert_name}\n/status\n/pod-doublecheck {>0: new_refresh_time_sec/<=0: is to disable}", nil)
 			log.Info("/help requested from Chat ID: %v", message.Chat.ID)
 		case LIST:
 			alist := GetAlerts()
