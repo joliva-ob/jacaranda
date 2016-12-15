daf1fc53bd00aba3132538a144573b4fda04df97
Joan Oliva <joliva@oneboxtm.com>
2016-12-15 12:39:14 +0100

    send pod-doublecheck time into param <time>

--- pod-doublecheck.go
+++ pod-doublecheck.go
@@ -11,9 +11,13 @@ import (
 
 func processNewPodDoublecheckRefreshtime(newRefreshtime int, message *telebot.Message) {
 
-	res, err := sendHttpRequest("PUT", config.Pod_doublecheck_url+strconv.Itoa(newRefreshtime), nil, nil)
+	url := config.Pod_doublecheck_url+strconv.Itoa(newRefreshtime)
+	params := make(map[string]string)
+	params["time"] = strconv.Itoa(newRefreshtime)
+	res, err := sendHttpRequest("PUT", url, params, nil)
+
 	if err != nil {
-		bot.SendMessage(message.Chat, "Error changing pod-doublecheck refreshtime to "+strconv.Itoa(newRefreshtime)+": "+res.Status, nil)
+		bot.SendMessage(message.Chat, "Error changing pod-doublecheck refreshtime to "+strconv.Itoa(newRefreshtime)+" -> "+err.Error(), nil)
 		log.Infof("Error changing pod-doublecheck refreshtime to %v: %v", newRefreshtime, res.Status)
 	} else {
 		bot.SendMessage(message.Chat, "Pod-Doublecheck refresh time changed to: "+strconv.Itoa(newRefreshtime), nil)
@@ -28,9 +32,8 @@ func sendHttpRequest(method string, url string, params map[string]string, header
 	req, _ := http.NewRequest(method, url, nil)
 //	req.Header.Set("Authorization", "Bear 1736cc7f-7c60-4576-b851-b7b3630cfeab")
 	q := req.URL.Query()
-//	q.Add("chat_id", chatId)
+	q.Add("time", params["time"])
 	req.URL.RawQuery = q.Encode()
-
 	res, err := client.Do(req)
 
 	if err != nil {
--- pod-doublecheck_test.go
+++ pod-doublecheck_test.go
@@ -5,16 +5,22 @@ import (
 
 	"net/http"
 	"testing"
-
+	"strconv"
 	"github.com/stretchr/testify/assert"
 
+	"fmt"
 )
 
 
 
 func TestChangePodDoubleChecktime(t *testing.T) {
 
-	res, err := testSendHttpRequest("PUT", "http://10.1.22.50:30920/refreshtime?-1", nil, nil)
+	go startServer(9090)
+
+	params := make(map[string]string)
+	params["time"] = strconv.Itoa(20)
+	// res, err := testSendHttpRequest("PUT", "http://10.1.22.50:30920/refreshtime?", params, nil)
+	res, err := testSendHttpRequest("PUT", "http://localhost:9090/refreshtime?", params, nil)
 
 	if res != nil {
 		assert.NotNil(t, res, "Http response is %v", res)
@@ -36,8 +42,9 @@ func testSendHttpRequest(method string, url string, params map[string]string, he
 	req, _ := http.NewRequest(method, url, nil)
 	//	req.Header.Set("Authorization", "Bear 1736cc7f-7c60-4576-b851-b7b3630cfeab")
 	q := req.URL.Query()
-	//q.Add("", "-1")
+	q.Add("time", params["time"])
 	req.URL.RawQuery = q.Encode()
+	fmt.Printf("Url to send http request: %v", req.URL.RawQuery)
 
 	res, err := client.Do(req)
 
@@ -46,4 +53,14 @@ func testSendHttpRequest(method string, url string, params map[string]string, he
 	}
 
 	return res, nil
+}
+
+
+func startServer(port int) {
+	http.HandleFunc("/", fooHandler)
+	http.ListenAndServe(":" + strconv.Itoa(port), nil)
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("time param received is: " + r.URL.Query().Get("time"))
 }
